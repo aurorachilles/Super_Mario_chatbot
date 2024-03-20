42133e4a45d0b3ebfe2d7aac16a0d6e099656940
Vishal Pattanaik <vishal.pattanaik05@gmail.com>
2024-03-20 23:49:38 +0530

    finished

--- save.py
+++ save.py
@@ -2,8 +2,9 @@ def save_conversation(convo):
     f = open("conversation.txt", "w+")
     for i, con in enumerate(convo, start=2):
         if i % 2 == 0:
-            f.write("Super Mario : " + con + "\n")
+            f.write("Super Mario : " + con['content'] + "\n")
         else:
-            f.write("User : " + con + "\n")
+            f.write("User : " + con['content'] + "\n")
     f.close()
+
     print("Conversation saved!")
